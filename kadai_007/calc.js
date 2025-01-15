04f132561a6a1783bf271f98eba74e6b15862131
maruyama111 <uyuhon@gmail.com>
2025-01-16 08:38:35 +0900

    JavaScript基礎の課題提出_kadai_007

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,17 +1,14 @@
 let num = 15;
 
-if(num%3 == 0 && num%5 != 0){
+if(num%3 == 0 && num%5 == 0){
+    console.log('15の倍数です');
+    }
+else if(num%3 == 0){
     console.log('3の倍数です');
     }
-
-if(num%5 == 0 && num%3 != 0){
+else if(num%5 == 0){
     console.log('5の倍数です');
     }
-
-if(num%3 == 0 && num%5 == 0){
-    console.log('3と5の倍数です');
-    }
-
-if(num%3 != 0 && num%5 != 0){
+else{
     console.log(num);
     }
